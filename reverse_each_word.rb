0039fe5550b1dbeb4c13355a3a04b1ad9d57650f
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-08 16:29:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   #code here 
   new_array1 = string.split(" ")
-  new_array.each do |word|
+  new_array.collect do |word|
     word.reverse
   
 end #end reverse_each_word
