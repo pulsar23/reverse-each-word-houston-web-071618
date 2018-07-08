30ff9309ce568a20feb04040e365a1521f1ec95f
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-08 16:37:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,16 @@
-def reverse_each_word(string)
-  #code here 
-  new_array1 = string.split(" ")
-  new_array.collect do |word|
-    word.reverse
-  
-end #end reverse_each_word
 
+def reverse_each_word(string)
+  
+  new_array1 = string.split(" ")
+  new_array2 = []
+  
+  new_array1.collect do |word|
+    new_word = word.reverse
+    puts new_word
+    new_array2.push(new_word)
+  end
+  
+  string2 = new_array2.join(" ")
+  return string2
+  
+end
